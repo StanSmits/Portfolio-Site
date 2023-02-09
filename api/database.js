fb21fe801f2c7541a52cc42cb8d058bca89c4275
Stan Smits <s1136644@student.hsleiden.nl>
2023-02-09 12:39:18 +0100

    Fix: spelling mistake in .env file

--- api/database.js
+++ api/database.js
@@ -5,5 +5,5 @@ module.exports = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABSE
+    database: process.env.DB_DATABASE
 })
