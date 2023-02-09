d32816e32943ca6a29fbfeb5d517fa5c3e3b975f
Stan Smits <s1136644@student.hsleiden.nl>
2023-02-09 13:55:04 +0100

    fix: remove comments and add margin left when screen size is small

--- app/src/components/CardsBody/CardsBody.tsx
+++ app/src/components/CardsBody/CardsBody.tsx
@@ -2,17 +2,9 @@ import { Children } from "react";
 import Cards from "../Cards/Cards";
 
 function CardsBody() {
-  // Flexbox for the cards
-  // When its mobile, it should be a column
-  // When its desktop, it should be a row
-  // Have the overlaying div be the color secondary
-  // padding within the cards
-  // margin between the cards
-  // margin between the cards and the footer
-
   return (
     <>
-      <div className="flex flex-col self-start md:justify-center md:items-center mb-10">
+      <div className="flex flex-col self-start md:justify-center md:items-center mb-10 m-10">
         <div className="container">
           <div className="bg-card-bg rounded-md mb-5 p-5 max-h-sm self-center justify-center md:self-start w-full">
             <Cards
