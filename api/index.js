06ecc1dd9f32df9e57ef6891c71435f11894b9d2
Stan Smits <s1136644@student.hsleiden.nl>
2023-02-09 12:08:17 +0100

    FEAT: Added api

---
+++ api/index.js
@@ -0,0 +1,51 @@
+const app = require("express")();
+require("dotenv").config();
+const cors = require("cors")();
+
+const db = require("./database");
+const PORT = process.env.PORT;
+
+app.use(cors);
+
+app.get("/education", async (req, res) => {
+    const results = await db.promise().query(`SELECT * FROM Education`)
+    console.log(results[0])
+    res.status(200).send(results[0])
+});
+
+app.post("/education", (req, res) => {
+  const { name, description, created_at } = req.body;
+  if (name && description && created_at) {
+    try {
+      db.promise().query(
+        `INSERT INTO Education VALUES('${name}','${description}','${created_at}')`
+      );
+      console.log('Added %d to the db', name)
+    } catch (err) {
+      console.error(err);
+    }
+  }
+});
+
+app.get("/projects", async (req, res) => {
+    const results = await db.promise().query(`SELECT * FROM Projects`)
+    console.log(results[0])
+    res.status(200).send(results[0])
+});
+
+app.post("/projects", (req, res) => {
+  // Check the authorisation
+  const { name, description, created_at, html_url } = req.body;
+  if (name && description && created_at && html_url) {
+    try {
+      db.promise().query(
+        `INSERT INTO Projects VALUES('${name}','${description}','${created_at}', '${html_url}')`
+      );
+      console.log('Added %d to the db', name)
+    } catch (err) {
+      console.error(err);
+    }
+  }
+});
+
+app.listen(PORT, () => console.log("Listening on port " + PORT));
