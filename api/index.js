45594d3ba4ac155ed712bfd767caee90b5dfdec6
Stan <79027654+StanSmits@users.noreply.github.com>
2023-02-10 01:28:34 +0100

    Fix: update database for admin panel

--- api/index.js
+++ api/index.js
@@ -1,49 +1,156 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 require("dotenv").config();
 const cors = require("cors")();
+const { v4: uuidv4 } = require("uuid");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const db = require("./database");
 const PORT = process.env.PORT;
 
 app.use(cors);
+app.use(express.json());
+app.use(express.urlencoded());
+
+const verifyToken = (req, res, next) => {
+  let token = req.header("Authorization");
+  const secret = process.env.TOKEN_SECRET;
+
+  if (!token || token === "undefined") {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
+
+  token = token.replace("Bearer ", "");
+
+  try {
+    const decoded = jwt.verify(token, secret);
+    req.user = decoded.user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ msg: "Token is not valid" });
+  }
+};
 
 app.get("/education", async (req, res) => {
-    const results = await db.promise().query(`SELECT * FROM Education`)
-    console.log(results[0])
-    res.status(200).send(results[0])
+  const results = await db.promise().query(`SELECT * FROM Education`);
+  console.log(results[0]);
+  res.status(200).send(results[0]);
 });
 
-app.post("/education", async (req, res) => {
-  const { name, description, created_at } = req.body;
+app.post("/education", verifyToken, async (req, res) => {
+  const { name, description, created_at, ended_at } = req.body;
   if (name && description && created_at) {
     try {
-      await db.promise().query(
-        `INSERT INTO Education VALUES('${name}','${description}','${created_at}')`
-      );
-      console.log('Added %d to the db', name)
+      await db
+        .promise()
+        .query(
+          `INSERT INTO Education (name, description, created_at, ended_at) VALUES('${name}','${description}','${created_at}', '${ended_at}')`
+        );
+      res.status(200).send("success");
     } catch (err) {
+      res.status(500).send("error");
+    }
+  }
+});
+
+app.delete("/education/:id", verifyToken, async (req, res) => {
+  id = req.params.id;
+  try {
+    await db.promise().query(`DELETE FROM Education WHERE id = ${id}`);
+    res.status(200).send("success");
+  } catch (err) {
+    res.status(500).send("error");
+  }
+});
+
+app.get("/projects", async (req, res) => {
+  const results = await db.promise().query(`SELECT * FROM Projects`);
+  console.log(results[0]);
+  res.status(200).send(results[0]);
+});
+
+app.post("/projects", verifyToken, async (req, res) => {
+  // Check the authorisation
+  const { name, description, created_at, ended_at, html_url } = req.body;
+  if (name && description && created_at && html_url) {
+    try {
+      await db
+        .promise()
+        .query(
+          `INSERT INTO Projects (name, description, created_at, ended_at, html_url) VALUES('${name}','${description}','${created_at}', '${ended_at}', '${html_url}')`
+        );
+      // Write a return message
+      res.status(200).send("success");
+    } catch (err) {
+      res.status(500).send("error");
       console.error(err);
     }
   }
 });
 
-app.get("/projects", async (req, res) => {
-    const results = await db.promise().query(`SELECT * FROM Projects`)
-    console.log(results[0])
-    res.status(200).send(results[0])
+app.delete("/projects/:id", verifyToken, async (req, res) => {
+  id = req.params.id;
+  try {
+    await db.promise().query(`DELETE FROM Projects WHERE id = ${id}`);
+    res.status(200).send("success");
+  } catch (err) {
+    res.status(500).send("error");
+  }
 });
 
-app.post("/projects", async (req, res) => {
-  // Check the authorisation
-  const { name, description, created_at, html_url } = req.body;
-  if (name && description && created_at && html_url) {
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  if (username && password) {
     try {
-      await db.promise().query(
-        `INSERT INTO Projects VALUES('${name}','${description}','${created_at}', '${html_url}')`
-      );
-      console.log('Added %d to the db', name)
+      const results = await db
+        .promise()
+        .query(`SELECT * FROM Users WHERE username = '${username}'`);
+      if (results[0].length === 0) {
+        res.status(404).send("User not found");
+      } else {
+        const user = results[0][0];
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (isPasswordCorrect) {
+          const token = jwt.sign(
+            { username: user.username },
+            process.env.TOKEN_SECRET
+          );
+          res.status(200).send({ token: token, username: user.username });
+        } else {
+          res.status(403).send("Incorrect password");
+        }
+      }
     } catch (err) {
-      console.error(err);
+      res.status(500).send("error");
+      console.log(err);
+    }
+  } else {
+    res.status(400).send("Missing username or password");
+  }
+});
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  if (username && password) {
+    try {
+      const results = await db
+        .promise()
+        .query(`SELECT * FROM Users WHERE username = '${username}'`);
+      if (results[0].length === 0) {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await db
+          .promise()
+          .query(
+            `INSERT INTO Users (uuid, username, password) VALUES('${uuidv4()}', '${username}', '${hashedPassword}')`
+          );
+        res.status(200).send("success");
+      } else {
+        res.status(403).send("User already exists");
+      }
+    } catch (err) {
+      res.status(500).send("error");
+      console.log(err);
     }
   }
 });
